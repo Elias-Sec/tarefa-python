b60814421c74d7c9a92e33b17c3839922928d34b
Elias-Sec <Wendell.infosec@gmail.com>
2026-05-07 15:47:57 -0300

    exe02 completo

--- exe02.py
+++ exe02.py
@@ -11,4 +11,14 @@ while True:
         print("Muito baixo! Tente novamente.")
     elif palpite > numero_secreto:
         print("Muito alto! Tente novamente.")
-    
+    elif tentativas > 7:
+        print(f"Infelizmente, você excedeu o número máximo de tentativas. O número secreto era {numero_secreto}.")
+        break
+    else:
+        print(f"Parabéns! Você acertou o número {numero_secreto} em {tentativas} tentativas!")
+        break
+
+if tentativas <= 7:
+    print("Excelente! Você é um(a) adivinhador(a) talentoso(a)!")
+else:
+    print("Boa tentativa! Continue praticando para melhorar suas habilidades de adivinhação.")
